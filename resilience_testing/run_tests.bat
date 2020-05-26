f7e093f2a946c8ee6490380c3e165bdd0b7149df
ThomasTog <hrc720@alumni.ku.dk>
2020-05-26 16:18:21 +0200

    done 4 today

--- resilience_testing/run_tests.bat
+++ resilience_testing/run_tests.bat
@@ -1,5 +1,5 @@
-python experiment_controller.py 100 100 .\versions .\data .\results 0 1
-python experiment_controller.py 100 100 .\versions .\data .\results 0 2
-python experiment_controller.py 100 100 .\versions .\data .\results 0 3
-python experiment_controller.py 100 100 .\versions .\data .\results 0 4
-python experiment_controller.py 100 100 .\versions .\data .\results 0 5
+python experiment_controller.py 100 250 .\versions .\data .\results 0 1
+python experiment_controller.py 100 250 .\versions .\data .\results 0 2
+python experiment_controller.py 100 250 .\versions .\data .\results 0 3
+python experiment_controller.py 100 250 .\versions .\data .\results 0 4
+python experiment_controller.py 100 250 .\versions .\data .\results 0 5
